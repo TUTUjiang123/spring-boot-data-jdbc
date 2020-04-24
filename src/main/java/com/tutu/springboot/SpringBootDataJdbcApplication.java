471e0bdf508d46d807b2d8cef7bdcb370dbfdf57
TUTUJiang123 <tutubaobeio00>
2020-04-24 14:47:23 +0800

    first commit

--- src/main/java/com/tutu/springboot/SpringBootDataJdbcApplication.java
+++ src/main/java/com/tutu/springboot/SpringBootDataJdbcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDataJdbcApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDataJdbcApplication.class, args);
     }
